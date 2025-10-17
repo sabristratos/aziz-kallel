dc8fa95f206e6e661de85c55397657238b3201f0
Sabri <sabri@stratosdigital.io>
2025-10-17 19:25:06 +0100

    Updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             LocaleMiddleware::class,
         ]);
         $middleware->trustProxies('*');
-
+        $middleware->validateCsrfTokens(except: [
+            'livewire/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
